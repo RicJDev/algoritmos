6f3709968f6339786ff3ac2bb21c9987fd0e1aae
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2025-08-19 08:31:31 -0400

    agregando demostracion de los algoritmos en Python

---
+++ algoritmos-1/basico/demostracion-en-python/operaciones_matematicas.py
@@ -0,0 +1,14 @@
+suma = 1 + 2
+resta = 3 - 1
+multiplicacion = 2 * 4
+division = 4 / 2
+modulo = 10 % 2
+potencia = 3**4
+
+
+print("La suma de 1 y 2:", suma)
+print("La diferencia entre 3 y 1:", resta)
+print("El producto de 2 por 4:", multiplicacion)
+print("El cociente de 4 entre 2:", division)
+print("El modulo de 10 entre 2:", modulo)
+print("Potencia de 3 elevado a 4:", potencia)
---
+++ algoritmos-1/basico/demostracion-en-python/variables_y_tipos.py
@@ -0,0 +1,16 @@
+numero_real = 1.2
+numero_entero = 12
+mi_caracter = "a"
+mi_cadena_de_texto = "Hola, mundou!"
+nombre = ""
+mi_booleano = True
+
+
+print("Este es un mensaje sumamente importante")
+
+
+print("Ingrese su nombre")
+nombre = input()
+
+
+print("Hola. Bienvenido,", nombre)
